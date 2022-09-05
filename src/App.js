9514e363db48cc341f6febd62056f22b50bd292e
jashanshah33 <jashanshah33@gmail.com>
2022-09-05 06:53:07 -0400

    css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React from "react";
+import Menu from "./menu";
 
-function App() {
+class App extends React.Component {
+
+    constructor(){
+      super()
+      this.state={
+        menu:[
+          {cover: "Cover Flow"},
+          {music: "Music"},
+          {games: "Games"},
+          {setting: "Setting"}
+        ] 
+      }
+    }
+
+  render(){
+
+    const{menu} = this.state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="screen">
+        <div className="left-Screen">
+          <div className="innerText">
+        <h3>iPod.js</h3>
+      {
+        menu.map( (d) => {
+          return <Menu 
+          menu={d}
+          />
+        })
+      }
+      </div>
+
+        </div>
+        <div className="right-screen"></div>
+      </div>
+      <div className="controllers">
+        <button className="top">MENU</button>
+        <button className="left">
+        <i class="fa-solid fa-backward-fast"></i>
+        </button>
+        <div className="enter"></div>
+        <button className="right">
+        <i class="fa-solid fa-forward-fast"></i>
+        </button>
+        <button className="bottom">
+        <i style={{marginRight:2}} class="fa-solid fa-play"></i>      
+        <i class="fa-solid fa-pause"></i>
+         {/* <img src="https://cdn-icons-png.flaticon.com/512/5725/5725942.png" /> */}
+        </button>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
